dfdffd6890f1e387ddb4d07b7056a3481e4aa7b8
tarioto <timarioto@gmail.com>
2016-10-14 15:48:25 -0700

    Build out links controller

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,9 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+
+  Links.getAll().then(function(resp) {
+    $scope.data.links = resp;
+  });
 });
