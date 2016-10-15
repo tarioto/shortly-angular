436efa1d131cdd77781fb566a507c57bbc4bcab9
tarioto <timarioto@gmail.com>
2016-10-14 19:46:31 -0700

    Require authorization to view pages

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,10 +1,14 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, $location, Auth) {
   // Your code here
   $scope.data = {};
 
-  Links.getAll().then(function(resp) {
-    $scope.data.links = resp;
-  });
+  if (Auth.isAuth()) {
+    Links.getAll().then(function(resp) {
+      $scope.data.links = resp;
+    });
+  } else {
+    $location.path('/signin');
+  }
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,10 +1,14 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
   // Your code here
   $scope.link = {};
 
-  $scope.addLink = function(link) {
-    Links.addOne(link);
-  };
+  if (Auth.isAuth()) {
+    $scope.addLink = function(link) {
+      Links.addOne(link); 
+    };
+  } else {
+    $location.path('/signin');
+  }
 });
