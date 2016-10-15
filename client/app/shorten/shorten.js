d31990b906f3d26cff3c60bd9c5d22b0bd29bcbc
Tim Arioto <timarioto@gmail.com>
2016-10-15 16:13:12 -0700

    Implement checker for URL

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,6 +3,18 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links, Auth) {
   // Your code here
   $scope.link = {};
+
+  $scope.message;
+
+  $scope.isValid = function() {
+    if ($scope.link.url === 'http://') {
+      $scope.message = '';
+    } else if ($scope.link.url === '') {
+      $scope.message = 'enter URL';
+    } else {
+      $scope.message = 'not valid URL';
+    }
+  };
   $scope.addLink = function(link) {
     Links.addOne(link);
   };
